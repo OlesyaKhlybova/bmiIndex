3ed69748f7de6ede7a5c03783b323950ed64dd9e
OlesyaKhlybova <133571945+OlesyaKhlybova@users.noreply.github.com>
2023-06-21 18:10:11 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,8 @@
 public class BmiService {
     public int calculate(double weight, double height) {
         double bmiIndex = weight / Math.pow(height, 2);
+        // weight вес в кг
+        // height рост в метрах
         return (int) bmiIndex;
     }
 }
